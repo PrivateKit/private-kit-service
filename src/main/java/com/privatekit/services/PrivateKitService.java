7fcfdb419dd4a23b16bec3c992da03e51ce02fe3
Prithvi Krishnan Padmanabhan <ppadmanabhan@salesforce.com>
2020-04-19 22:26:07 -0700

    Adding swagger configuration

---
+++ src/main/java/com/privatekit/services/PrivateKitService.java
@@ -0,0 +1,11 @@
+package com.privatekit.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivateKitService {
+    public static void main(String[] args) {
+        SpringApplication.run(PrivateKitService.class, args);
+    }
+}
---
+++ src/main/java/com/privatekit/services/controller/WaypointsController.java
@@ -0,0 +1,15 @@
+package com.privatekit.services.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WaypointsController {
+	@PostMapping("/v1.0/{app_id}/waypoints")
+	public ResponseEntity<String> create(@PathVariable("app_id") String app_id) {
+		String body = "Waypoints inserted successfully";
+		return ResponseEntity.ok(body);
+	}
+}
